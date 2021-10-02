e8dcae73b62b7cd2308db3bea9f7eaa7e331d306
Danit <danishagan@gmail.com>
2021-10-02 16:10:52 +0300

    Create end-of-game screen
    * feat: Add caption: "Oh No Game Over"
    * feat: Display the points scheme of the player
    * feat: Add "Play Again" button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public Transform GameOverPanel;
+
     public float scoreFactor;
     public float Score;
 
+    public Text GameOverScore;
     public Text LivesText;
     public Text ScoreText;
 
@@ -26,17 +30,32 @@ public class GameManager : MonoBehaviour
     {
         LivesText.text = "x " + ourPlayer.Lives;
 
-        if(ourPlayer.Lives == 0)
+        if(ourPlayer.Lives == 0 && ourPlayer)
         {
             Destroy(ourPlayer);
+
+            ShowGameOverPanel();
         }
 
         UpdateScore();
+    }
+
+    void ShowGameOverPanel()
+    {
+        Time.timeScale = 0;
+        GameOverScore.text += Score.ToString("F0");
+        GameOverPanel.gameObject.SetActive(enabled);
     }
 
     void UpdateScore()
     {
         Score += Time.deltaTime * scoreFactor;
         ScoreText.text = "Score : " + Score.ToString("F0");
+    }
+
+    public void PlayAgain()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
